270da0cad0142ed09b3e9cd831db70411c8e5470
Andrey Zherikov <andrey.zherikov@gmail.com>
2022-03-16 20:23:36 -0400

    Default subcommand

---
+++ examples/sub_commands/default/app.d
@@ -0,0 +1,46 @@
+import argparse;
+import std.stdio: writeln;
+import std.sumtype: SumType, match;
+
+
+struct sum {}
+struct min {}
+struct max
+{
+    string foo; // --foo argument
+}
+
+struct Program
+{
+    int[] numbers;  // --numbers argument
+
+    // SumType indicates sub-command
+    // Default!T marks T as default command
+    SumType!(sum, min, Default!max) cmd;
+}
+
+// This mixin defines standard main function that parses command line and calls the provided function:
+mixin CLI!Program.main!((prog)
+{
+    static assert(is(typeof(prog) == Program));
+
+    prog.cmd.match!(
+        (.max m)
+        {
+            import std.algorithm: maxElement;
+            writeln("max = ", prog.numbers.maxElement, " foo = ", m.foo);
+        },
+        (.min)
+        {
+            import std.algorithm: minElement;
+            writeln("min = ", prog.numbers.minElement);
+        },
+        (.sum)
+        {
+            import std.algorithm: sum;
+            writeln("sum = ", prog.numbers.sum);
+        }
+    );
+
+    return 0;
+});
