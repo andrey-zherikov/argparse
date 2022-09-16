e514627808ee1c758f44c28305f9d9c1e9bdcec9
Andrey Zherikov <andrey.zherikov@gmail.com>
2022-09-16 17:58:05 -0400

    Add support of custom types (#77)
    (cherry picked from commit 51dd8de7b12c705d54a6130aa74426e948eb5fd5)

--- source/argparse/internal/package.d
+++ source/argparse/internal/package.d
@@ -726,7 +726,16 @@ if(!is(T == void))
         );
     }
     else
-        static assert(false, "Type is not supported: " ~ T.stringof);
+    {
+        alias DefaultValueParseFunctions = ValueParseFunctions!(
+            void,   // pre process
+            void,   // pre validate
+            void,   // parse
+            void,   // validate
+            void,   // action
+            void    // no-value action
+        );
+    }
 }
 
 unittest
