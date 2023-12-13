da85d2f771df713637bba56be156ef3c29ee6f1e
Nickolay Bukreyev <buknik95@yandex.ru>
2023-12-14 01:37:40 +0700

    Prettify `tools/deps.sh`

--- tools/deps.sh
+++ tools/deps.sh
@@ -1,5 +1,19 @@
-cat <<EOF | dot -Tpng > deps.png
-digraph {
-$(egrep -rw 'import +argparse' --include=*.d source | sed 's/public \+//; s/^source\/\(.\+\)\.d: *import \([^:; ]\+\).*/"\2" -> "\1"/; s/\//./g; s/\.package//g')
-}
-EOF
+#!/usr/bin/env bash
+
+set -euo pipefail
+IFS=$'\t\n'
+[[ $# -le 1 ]] || exit 2
+
+{
+    cd -- "${0%[/\\]*}/../source"
+
+    echo 'digraph {'
+
+    grep -rw 'import \+argparse' --include='*.d' | sed -E '
+    s/^(.+)\.d: *(public +)?import +([^:; ]+).*/"\3" -> "\1"/
+    s|/|.|g
+    s/\.package//g
+    ' | sort -u
+
+    echo '}'
+} | dot -Tsvg -o "${1-deps.svg}"
