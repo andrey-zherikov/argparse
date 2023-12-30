e2e04244e8ec80fab61b823e9aeab50e03c6e84d
Andrey Zherikov <andrey.zherikov@gmail.com>
2023-12-29 22:03:56 -0500

    Make Success and Error public

--- source/argparse/result.d
+++ source/argparse/result.d
@@ -3,30 +3,42 @@ module argparse.result;
 
 struct Result
 {
+    ////////////////////////////////////////////////////////////////
+    /// Public API
+    ////////////////////////////////////////////////////////////////
     int  resultCode;
-
-    package enum Status { failure, success, unknownArgument };
-    package Status status;
-
-    package string errorMsg;
-
-    package const(string)[] suggestions;
-
-    package static enum Failure = Result(1, Status.failure);
-    package static enum Success = Result(0, Status.success);
-    package static enum UnknownArgument = Result(0, Status.unknownArgument);
 
     bool opCast(T : bool)() const
     {
         return status == Status.success;
     }
 
-    package static auto Error(A...)(A args)
+
+    static enum Success = Result(0, Status.success);
+
+    static auto Error(T...)(string msg, T extraArgs)
+    {
+        return Error(1, msg, extraArgs);
+    }
+    static auto Error(T...)(int resultCode, string msg, T extraArgs)
     {
         import std.conv: text;
 
-        return Result(1, Status.failure, text!A(args));
+        return Result(resultCode, Status.error, text(msg, extraArgs));
     }
+
+    ////////////////////////////////////////////////////////////////
+    /// Private API
+    ////////////////////////////////////////////////////////////////
+
+    package enum Status { error, success, unknownArgument };
+    package Status status;
+
+    package string errorMsg;
+
+    package const(string)[] suggestions;
+
+    package static enum UnknownArgument = Result(0, Status.unknownArgument);
 
     version(unittest)
     {
@@ -34,7 +46,7 @@ struct Result
         {
             import std.algorithm: canFind;
 
-            if(cast(bool) this)
+            if(status != Status.error)
                 return false;   // success is not an error
 
             foreach(s; text)
